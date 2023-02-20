3cbc8f29fb626f16b538586f84067a643d2ba6a2
Angel <48069264+velk20@users.noreply.github.com>
2023-02-20 17:07:44 +0200

    @ControllerAdvice for Trimming every HTTP DTO attributes

--- src/main/java/com/zoomania/zoomania/exceptionHandling/ZoomaniaControllerAdvice.java
+++ src/main/java/com/zoomania/zoomania/exceptionHandling/ZoomaniaControllerAdvice.java
@@ -1,10 +1,20 @@
 package com.zoomania.zoomania.exceptionHandling;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 
 @ControllerAdvice
 public class ZoomaniaControllerAdvice {
+    //Trimming every HTTP DTO attributes
+    @InitBinder
+    public void initBinder ( WebDataBinder binder )
+    {
+        StringTrimmerEditor stringtrimmer = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringtrimmer);
+    }
     @ExceptionHandler({Exception.class})
     public String handleError() {
         return "redirect:error";
