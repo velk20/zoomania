059dcff7d3839ca00cae197bd4303de2a8bdc63e
Angel <48069264+velk20@users.noreply.github.com>
2023-02-26 18:01:23 +0200

    User's offers

--- build/resources/main/static/js/users.js
+++ build/resources/main/static/js/users.js
@@ -40,15 +40,16 @@ fetch(`http://localhost:8080/api/users?pageSize=${pageSize}&pageNo=${pageNo}&sor
               <tr>
                 <td>
                   <img src="https://bootdey.com/img/Content/user_1.jpg" alt="">
-                  <a href="#" class="user-link">${user.firstName + ' ' + user.lastName}</a>
-                  <span class="user-subhead">${user.userRoles}</span>
+                  <a href="/users/profile/${user.username}" class="user-link">${user.firstName + ' ' + user.lastName}</a>
+                  <span class="user-subhead">ROLES:[${user.userRoles}]</span>
                 </td>
                 <td><h6>${user.phone}</h6></td>
                 <td class="text-center">
-                <h6>
-                ${user.active === true ? 'Yes' : 'No'}
                 
-</h6>
+                <h6 class="userIsActive" >
+                       ${user.active === true ? 'Yes' : 'No'}
+                </h6>
+               
                 </td>
                 <td>
                   <a href="mailto:${user.email}"><h6>${user.email}</h6></a>
@@ -75,7 +76,6 @@ fetch(`http://localhost:8080/api/users?pageSize=${pageSize}&pageNo=${pageNo}&sor
                 </td>
               </tr>
             `;
-
             }
 
         }
@@ -93,6 +93,9 @@ fetch(`http://localhost:8080/api/users?pageSize=${pageSize}&pageNo=${pageNo}&sor
         let csrfHeaderNameDeleteOffer = document.head.querySelector('[name=_csrf_header]').content
         let csrfHeaderValueDeleteOffer = document.head.querySelector('[name=_csrf]').content
         deleteUserBtns.forEach(b=>b.addEventListener("click", onDeleteUserBtn));
+
+
+
         async function onDeleteUserBtn(event){
             let dataUsername = event.currentTarget.getAttribute('data-username');
             if (confirm("Are you sure you want to delete this user," +
