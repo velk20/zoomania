e174f6330f03dda5156e4079e87e8e71ad57c576
Angel Mladenov <a.mladenov@seeburger.com>
2024-02-27 20:52:23 +0200

    commit

--- src/main/java/com/zoomania/zoomania/exceptions/exceptionHandling/ZoomaniaControllerAdvice.java
+++ src/main/java/com/zoomania/zoomania/exceptions/exceptionHandling/ZoomaniaControllerAdvice.java
@@ -1,4 +1,4 @@
-package com.zoomania.zoomania.exceptionHandling;
+package com.zoomania.zoomania.exceptions.exceptionHandling;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
